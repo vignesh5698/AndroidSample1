bc1c77ff099a3fdc39c3254d1b38851ba084a6b5
root <root@localhost.localdomain>
2017-11-25 23:02:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.root.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1;
+        b1=(Button)findViewById(R.id.bt1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Second.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/root/myapplication/Second.java
@@ -0,0 +1,15 @@
+package com.example.root.myapplication;
+
+import android.os.Bundle;
+
+/**
+ * Created by root on 11/25/17.
+ */
+
+public class Second extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.l1);
+    }
+}
